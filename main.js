1b58fa0799a62297321e55fa36879deb54c9c59f
Muhammad Raffy Akbarsyah <89379922+raffyakbarsyah@users.noreply.github.com.>
2025-06-25 22:46:19 +0700

    Fixed typo in error text content

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ let currentInput = '';
 buttons.forEach(button => (
   button.addEventListener('click', () => {
     let value = button.textContent.trim();
-
     if (!["AC", "⌫", "="].includes(value)) {
-    if (currentInput.length >= 10) {
-      let error = document.querySelector(".errorMessage");
-      error.textContent = "Limit Hit"
-      return;
+      if (currentInput.length >= 10) {
+        const error = document.querySelector(".errorMessage");
+        error.textContent = "Limit: Only 10 digits allowed (up to billions).";
+        return;
+      }
+
+      currentInput += value;
+      display.textContent = currentInput;
     }
-    currentInput += value;
-    display.textContent = currentInput;
-  }
 
     if (value === "AC") {
       currentInput = '';
       display.textContent = "0";
+      document.querySelector(".errorMessage").textContent = ""; 
     }
 
     if (value === "⌫") {
-      currentInput = currentInput.slice(0 ,-1);
+      currentInput = currentInput.slice(0, -1);
       display.textContent = currentInput || "0";
+      document.querySelector(".errorMessage").textContent = "";
     }
-
-
   })
 ))
