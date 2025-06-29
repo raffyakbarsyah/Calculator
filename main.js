309475074b1f0b2c308cbb6b538585727b912eba
Muhammad Raffy Akbarsyah <89379922+raffyakbarsyah@users.noreply.github.com.>
2025-06-29 23:37:32 +0700

    Fixed percentage bugs, and added operator display functionality

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let operator = '';
 buttons.forEach(button => (
   button.addEventListener('click', () => {
     let value = button.textContent.trim();
-    if (!["AC", "⌫", "="].includes(value)) {
+    if (!["AC", "⌫", "=", "x", "+", "-", "÷", "%"].includes(value)) {
       if (currentInput.length >= 10) {
         const error = document.querySelector(".errorMessage");
         error.textContent = "Limit: Only 10 digits allowed (up to billions).";
@@ -31,8 +31,20 @@ buttons.forEach(button => (
       }
     
     if (value === "%") {
+      if (currentInput === '') return;
       currentInput = parseFloat(currentInput);
       display.textContent = currentInput / 100;
     }
+
+    if (["x", "+", "-", "÷"].includes(value)) {
+      if (currentInput === '') return;
+      firstNumber = parseFloat(currentInput);
+      operator = value;
+      display.textContent = currentInput + ' ' + value;
+      currentInput = '';
+      return;
+    }
+
+  
   }  
-))
+)))
